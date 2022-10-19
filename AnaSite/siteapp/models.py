923f117686c692bf02ee8ae1175fc6a4a54aa544
Hazar Koc <hazark41@gmail.com>
2022-10-20 01:28:52 +0300

    Bilgisayar table created, new migrations, new Bilgisayar variables

--- AnaSite/siteapp/models.py
+++ AnaSite/siteapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Bilgisayar(models.Model):
+    bilgisayarId = models.AutoField(primary_key=True)
+    marka = models.CharField(max_length = 50)
+    model = models.CharField(max_length = 50)
+    ram = models.IntegerField()
+    ssd = models.IntegerField()
+    
+    
