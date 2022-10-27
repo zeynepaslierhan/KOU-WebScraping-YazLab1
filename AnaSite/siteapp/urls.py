7891005e84aa3f758e47fb4b930f6520f49c9a46
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-10-27 22:04:17 +0300

    .

--- AnaSite/siteapp/urls.py
+++ AnaSite/siteapp/urls.py
@@ -7,6 +7,6 @@ from bson.objectid import ObjectId
 urlpatterns = [
     path('', views.pc_list, name='list-pc'),
     path('byıd<int:index>/', views.product_page, name='product_page'),
-    path('byBrand/<str:brand>/', views.productsList_byBrand , name='productsList_byBrand')
+    path('byBrand/<str:brand>/', views.productsList_byBrand , name='productsList_byBrand'),
     path('byWeb/<str:web>/', views.productsList_byWeb , name='productsList_byWeb')
 ]
